bf0812a7da7d558bbc8e944dc90d8b02a82fc71f
totox1000 <101885423+totox1000@users.noreply.github.com>
2022-03-31 14:50:03 -0300

    Add files via upload

---
+++ BECERRA_ANDRES_JSPREDICION.js
@@ -0,0 +1,24 @@
+function mybirthyearfunc () {
+    console.log("i was born in " + 1980);
+}
+// i was born in 1980
+
+// birthyearinput will be: 1980
+function myBirthYearFunc(birthyearinput){
+    console.log("i was born " + birthyearinput);
+}
+// i was born in 1980
+
+// calling this function with: num1 = 10 and var num2 = 20
+function add(num1, num2){
+    console.log("¡Summing numbers!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+
+// Summing numbers!
+// num1 is : 10
+// num2 is : 20
+// 30
